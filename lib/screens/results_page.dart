5de667cd8a062f18fc2b40bce155c5e74720f6f9
aliaaosama23 <engaliaaosama@gmail.com>
2022-01-28 09:04:19 +0200

    allow app for portrait mode only

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -19,95 +19,97 @@ class ResultsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 15,
-              vertical: 25,
-            ),
-            child: Text(
-              'Your Results',
-              style: TextStyle(
-                fontSize: 40,
-                color: kWhiteTextColor,
-                fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 25,
+              ),
+              child: Text(
+                'Your Results',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: kWhiteTextColor,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 5,
-            child: ReusableCard(
-              cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    resultText.toUpperCase(),
-                    style: const TextStyle(
-                      color: kGreenTextColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ),
-                  ),
-                  Text(
-                    result.toStringAsFixed(1),
-                    style: const TextStyle(
-                      color: kWhiteTextColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 80,
-                      letterSpacing: 2,
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 30,
-                      vertical: 20,
-                    ),
-                    child: Text(
-                      'You have a normal body weight ',
-                      style: TextStyle(
-                        color: kWhiteTextColor,
+            Expanded(
+              flex: 5,
+              child: ReusableCard(
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      resultText.toUpperCase(),
+                      style: const TextStyle(
+                        color: kGreenTextColor,
+                        fontWeight: FontWeight.bold,
                         fontSize: 30,
                       ),
-                      textAlign: TextAlign.center,
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // save the result
-                    },
-                    child: const Text(
-                      'SAVE RESULT',
-                      style: TextStyle(
-                        fontSize: 20,
+                    Text(
+                      result.toStringAsFixed(1),
+                      style: const TextStyle(
+                        color: kWhiteTextColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 80,
                         letterSpacing: 2,
                       ),
                     ),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        kInActiveCardColor,
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 30,
+                        vertical: 20,
                       ),
-                      padding: MaterialStateProperty.all(
-                        const EdgeInsets.symmetric(
-                          horizontal: 70,
-                          vertical: 20,
+                      child: Text(
+                        'You have a normal body weight ',
+                        style: TextStyle(
+                          color: kWhiteTextColor,
+                          fontSize: 30,
                         ),
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                  )
-                ],
+                    ElevatedButton(
+                      onPressed: () {
+                        // save the result
+                      },
+                      child: const Text(
+                        'SAVE RESULT',
+                        style: TextStyle(
+                          fontSize: 20,
+                          letterSpacing: 2,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          kInActiveCardColor,
+                        ),
+                        padding: MaterialStateProperty.all(
+                          const EdgeInsets.symmetric(
+                            horizontal: 70,
+                            vertical: 20,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          CustomButton(
-            btnText: 'Re-Calculate',
-            onPress: () {
-              Navigator.pop(context);
-            },
-          )
-        ],
+            CustomButton(
+              btnText: 'Re-Calculate',
+              onPress: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
